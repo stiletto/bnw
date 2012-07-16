f61afd107b9f1ff941871c5ccf9055f49e079d7a
Stiletto <blasux@blasux.ru>
2012-07-17 00:56:32 +0400

    stat

--- bnw_handlers/command_stat.py
+++ bnw_handlers/command_stat.py
@@ -7,6 +7,6 @@ import bnw_core.bnw_objects as objs
 
 @defer.inlineCallbacks
 def cmd_stat(request):
-    messages = list(x.doc for x in (yield objs.StatMessages.find({},limit=500)))
-    comments = list(x.doc for x in (yield objs.StatComments.find({},limit=500)))
+    messages = list(x.doc for x in (yield objs.StatMessages.find({})))
+    comments = list(x.doc for x in (yield objs.StatComments.find({})))
     defer.returnValue(dict(ok=True,desc='Oh, hai!',messages=messages,comments=comments,cache=3600,cache_public=True))
