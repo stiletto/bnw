5f0171754cceba17033fabac071f1d811ddbc044
Kagami Hiiragi <kagami@genshiken.org>
2012-07-21 03:46:26 +0400

    Rearrange js modules

--- bnw_web/static/dynamic.js
+++ bnw_web/static/dynamic.js
@@ -1,64 +1,3 @@
-var ws;
-var ws_addr;
-var opening = false;
-var last_try = (new Date).getTime();
-var tries_count = 0;
-var onmessage;
-
-function openws() {
-    if ("WebSocket" in window) {
-        ws = new WebSocket(ws_addr);
-    } else {
-        ws = new MozWebSocket(ws_addr);
-    }
-
-    ws.onopen = function() {
-        tries_count = 0;
-    }
-    ws.onclose = reopenws;
-    /*ws.onerror = function() {
-        if (ws.readyState == ws.OPEN) {
-            ws.close()
-        }
-        reopenws();
-    }*/
-    ws.onmessage = onmessage;
-
-    return ws;
-}
-
-function reopenws() {
-    if (!opening) {
-        opening = true;
-        _reopenws();
-    }
-}
-
-function _reopenws() {
-    var tnow = (new Date).getTime()
-    // 3 times for 5 seconds
-    if (tries_count < 3) {
-        if (tnow - last_try >= 5000) {
-            tries_count++;
-            opening = false;
-            openws();
-        } else {
-            setTimeout(_reopenws, 5000);
-        }
-    // 30 times for 1 minute
-    } else if (tries_count < 30) {
-        if (tnow - last_try >= 60000) {
-            tries_count++;
-            opening = false;
-            openws();
-        } else {
-            setTimeout(_reopenws, 60000);
-        }
-    } // In other cases seems like server in deep down, give up.
-    last_try = tnow;
-}
-
-
 var favicon_changed = false;
 var timeout_id;
 
@@ -161,7 +100,6 @@ function message_page_handler(e) {
         change_favicon();
     }
 }
-
 
 function api_call(func, args, verbose) {
     args["login"] = $.cookie("bnw_loginkey");
@@ -356,22 +294,13 @@ function add_message_page_actions(comment) {
 }
 
 
-var secure_connection = window.location.protocol == "https:";
 var is_auth_user = $.cookie("bnw_loginkey") != null;
 
 $(function() {
     switch (page_type) {
     case "main":
-        ws_addr = ((secure_connection ? "wss" : "ws" ) + "://" +
-                   websocket_base + "/ws?v=2");
-        onmessage = main_page_handler;
-        openws();
         break;
     case "message":
-        ws_addr = ((secure_connection ? "wss" : "ws" ) + "://" +
-                   websocket_base + window.location.pathname + "/ws?v=2");
-        onmessage = message_page_handler;
-        openws();
         if (is_auth_user) {
             add_message_page_actions();
         }
