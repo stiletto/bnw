a6a94cee4354ce00bb0fb69596819f930a1e8812
Stiletto <blasux@blasux.ru>
2012-11-11 20:59:57 +0400

    Different messages for on/off if already of or already on.

--- bnw_handlers/command_onoff.py
+++ bnw_handlers/command_onoff.py
@@ -10,18 +10,26 @@ import bnw_core.bnw_objects as objs
 @defer.inlineCallbacks
 def cmd_on(request):
     """ Включение доставки сообщений """
-    request.user['off']=False
-    _ = yield objs.User.mupdate({'name':request.user['name']},request.user,safe=True)
-    defer.returnValue(
-        dict(ok=True,desc='Welcome back!')
-    )
+    _ = yield objs.User.mupdate({'name':request.user['name']},{'$set':{'off':False}},safe=True)
+    if request.user['off']:
+        defer.returnValue(
+            dict(ok=True,desc='Welcome back!')
+        )
+    else:
+        defer.returnValue(
+            dict(ok=True,desc='Welcoooome baaaack, I said.')
+        )
 
 @require_auth
 @defer.inlineCallbacks
 def cmd_off(request):
     """ Выключение доставки сообщений """
-    request.user['off']=True
-    _ = yield objs.User.mupdate({'name':request.user['name']},request.user,safe=True)
-    defer.returnValue(
-        dict(ok=True,desc='C u l8r!')
-    )
+    _ = yield objs.User.mupdate({'name':request.user['name']},{'$set':{'off':True}},safe=True)
+    if request.user['off']:
+        defer.returnValue(
+            dict(ok=True,desc='See you later.')
+        )
+    else:
+        defer.returnValue(
+            dict(ok=True,desc='C u l8r!')
+        )
