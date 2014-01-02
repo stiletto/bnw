dd76b5cda0f46ec5e7952f08e94637929c0fa231
Stiletto <blasux@blasux.ru>
2014-01-02 21:35:16 +0400

    Log requests performance data. TODO: make this toggleable

--- bnw_web/base.py
+++ bnw_web/base.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import traceback
+from time import time
 from twisted.internet import defer
 from twisted.words.protocols.jabber.jid import JID
 import tornado.web
@@ -41,6 +42,7 @@ class BnwWebHandler(tornado.web.RequestHandler):
         d = defer.Deferred()
         d.addCallback(self.passargs, *args, **kwargs)
         d.addCallbacks(self.writeandfinish, self.errorfinish)
+        self.start_time = self.render_time = time()
         d.callback(self.respond)
 
     @tornado.web.asynchronous
@@ -48,6 +50,7 @@ class BnwWebHandler(tornado.web.RequestHandler):
         d = defer.Deferred()
         d.addCallback(self.passargs, *args, **kwargs)
         d.addCallbacks(self.writeandfinish, self.errorfinish)
+        self.start_time = self.render_time = time()
         d.callback(self.respond_post)
 
     def respond(self, *args, **kwargs):
@@ -67,6 +70,7 @@ class BnwWebHandler(tornado.web.RequestHandler):
         return super(BnwWebHandler, self).render(templatename, **args)
 
     def writeandfinish(self, text):
+        self.render_time = time()
         if isinstance(text, dict):
             try:
                 self.render(self.templatename, **text)
@@ -80,13 +84,19 @@ class BnwWebHandler(tornado.web.RequestHandler):
             if not self._finished:
                 self.write(text)
                 self.finish()
+        self.logperformance()
 
     def errorfinish(self, text):
+        self.render_time = time()
         text = text.getTraceback()
         handlerclass = self.__class__.__name__
         self.set_status(500)
         self.render(self.errortemplate, text=text, handlerclass=handlerclass)
         self.logperformance()
+
+    def logperformance(self):
+        end_time = time()
+        print 'PERFORMANCE',self.render_time-self.start_time, end_time-self.render_time, self.request.uri
 
     def static_url(self, path, include_host=None):
         if self.request.host in (config.webui_base, 'www'+config.webui_base) and self.request.protocol=="http":
