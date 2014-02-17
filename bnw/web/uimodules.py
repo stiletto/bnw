8d2a6c413a224b0d217e224513b12eb49ca1468d
Stiletto <blasux@blasux.ru>
2014-02-17 22:50:19 +0400

    Parametrize defargs with request

--- bnw/web/uimodules.py
+++ bnw/web/uimodules.py
@@ -5,17 +5,15 @@ from bnw.web.widgets import widgets
 
 class Message(tornado.web.UIModule):
     def render(self, msg, full=False, username=None, secure=False):
-        args = get_defargs()
+        args = get_defargs(self.handler)
         args['msg'] = msg
         args['full'] = full
         args['username'] = username
-        args['secure'] = secure
         return self.render_string('module-message.html', **args)
 
 
 class Comment(tornado.web.UIModule):
     def render(self, comment, secure=False):
-        args = get_defargs()
+        args = get_defargs(self.handler)
         args['comment'] = comment
-        args['secure'] = secure
         return self.render_string('module-comment.html', **args)
