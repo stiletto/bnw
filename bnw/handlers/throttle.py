48ad0f10112732061b3a77ddd907d7e4943c773a
Stiletto <blasux@blasux.ru>
2014-07-29 14:34:15 +0400

    WOW ANOTHER FIX ERMAHGERD

--- bnw/handlers/throttle.py
+++ bnw/handlers/throttle.py
@@ -10,7 +10,8 @@ import bnw.core.bnw_objects as objs
 def throttle_check(user):
     throttle = yield objs.Throttle.find_and_modify({'user': user},{'$inc':{'bucket':1}},upsert=True)
     if throttle and throttle.get('bucket',0) >= config.throttle_bucket_size:
-        raise BnwResponse('You are sending messages too fast! Please wait %d seconds before trying again.' % (throttle['bucket']*config.throttle_leak_speed,))
+        wtime = ( throttle['bucket'] - config.throttle_bucket_size + 1 ) * config.throttle_leak_speed
+        raise BnwResponse('You are sending messages too fast! Please wait %d seconds before trying again.' % (wtime,))
     defer.returnValue(throttle)
 
 @defer.inlineCallbacks
