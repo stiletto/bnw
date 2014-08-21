4c7f645cd2fcb7c22f3c52ab917691e8f0c4d6c1
Stiletto <blasux@blasux.ru>
2014-08-21 18:08:23 +0400

    Herp, derp, deploy into production

--- bnw/handlers/mapreduce.py
+++ bnw/handlers/mapreduce.py
@@ -6,6 +6,7 @@ import pymongo.errors
 from bnw.core.base import config
 import bnw.core.bnw_objects as objs
 from twisted.python import log
+import traceback
 
 @defer.inlineCallbacks
 def check_interval(job_name, interval):
@@ -16,33 +17,33 @@ def check_interval(job_name, interval):
             {'name': name, 'value': {'$lt': now-interval}},
             {'name': name, 'value': now}, upsert=True, w=1)
     except pymongo.errors.DuplicateKeyError:
-        return False
-    return upresult['n'] > 0
+        defer.returnValue(False)
+    defer.returnValue(upresult['n'] > 0)
 
 @defer.inlineCallbacks
 def do_mapreduce():
     now = time.time()
-    if yield check_interval('clubs', 6*3600):
+    if (yield check_interval('clubs', 6*3600)):
         yield objs.Message.map_reduce(
             map='function() { this.clubs.forEach(function(z) { emit(z, 1);}); }',
             reduce='function(k,vals) { var sum=0; for(var i in vals) sum += vals[i]; return sum; }',
             out={'replace': objs.Club.collection.collection_name}) # do not hardcode collection names
 
-    if yield check_interval('tags', 6*3600):
+    if (yield check_interval('tags', 6*3600)):
         yield objs.Message.map_reduce(
-            map='function() { this.tags.forEach(function(z) { emit(z, 1);}); }'
+            map='function() { this.tags.forEach(function(z) { emit(z, 1);}); }',
             reduce='function(k,vals) { var sum=0; for(var i in vals) sum += vals[i]; return sum; }',
             out={'replace': objs.Tag.collection.collection_name})
 
-    if yield check_interval('today', 300):
+    if (yield check_interval('today', 300)):
         start = now - 86400
         yield objs.Comment.map_reduce(
-            map='function() { emit(this.message, 1); }'
-            reduce='function(k,vals) { var sum=0; for(var i in vals) sum += vals[i]; return sum; }'
-            query={'date': {'$gte': start}}
+            map='function() { emit(this.message, 1); }',
+            reduce='function(k,vals) { var sum=0; for(var i in vals) sum += vals[i]; return sum; }',
+            query={'date': {'$gte': start}},
             out={'replace': objs.Today.collection.collection_name})
 
-    if yield check_interval('usertags', 86400):
+    if (yield check_interval('usertags', 86400)):
         yield objs.Message.map_reduce(
             map='''function () {
                 var user = this.user;
@@ -50,7 +51,7 @@ def do_mapreduce():
                     var tag = this.tags[i];
                     emit(user+" "+tag, {count:1, tag: tag, user: user});
                 }
-            }'''
+            }''',
             reduce='''function (k, vals) {
                 var sum = 0;
                 var user;
@@ -71,7 +72,7 @@ def do_mapreduce():
 @defer.inlineCallbacks
 def map_reduce_timer():
     try:
-        yield do_mapredue()
+        yield do_mapreduce()
     except:
         log.msg('MapReduce failed: %s ' % (traceback.format_exc(),))
     reactor.callLater(300, map_reduce_timer)
@@ -79,4 +80,4 @@ def map_reduce_timer():
 
 def setup_mapreduce():
     if config.mapreduce_enabled:
-        reactor.callLater(300, map_reduce_timer)
+        reactor.callLater(1, map_reduce_timer)
