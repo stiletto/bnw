stiletto/bnw

commit 4c7f4c4189e65e6d254c383ef0dceebe56ca0271

Remove username handling inconsistency
In https://bnw.im/p/QO2GG3, @0xd34df00d complained that BnW displays
usernames with leading "@" but requires not to use it it the commands.
This commit removes that inconsistency, making at sign optional in all
the commands that require username as argument.

bnw/formatting/linkshit_format.py
@@ -10,7 +10,7 @@ from libthumbor import CryptoURL
 from bnw.core.base import config
 from bnw.handlers.base import USER_RE
 
-_USER_RE = re.compile(ur"""(?:(?<=[\s\W])|^)@(%s)""" % USER_RE)
+_USER_RE = re.compile(ur"""(?:(?<=[\s\W])|^)%s""" % USER_RE)
 
 bnw_types = (
     ('msg', _MSG_RE, lambda m: (m.group(1),)),

bnw/handlers/base.py
@@ -3,12 +3,15 @@ import re
 from twisted.internet import defer, reactor
 import bnw.core.base
 
-USER_RE = ur'[0-9A-Za-z_-]+'
+# please don't remove the group from the regexp - it is used in
+# bnw/xmpp/parser_simplified.py
+USER_RE = ur'@?([0-9A-Za-z_-]+)'
 
 MESSAGE_RE = r'#?([0-9A-Za-z]+)'
 COMMENT_RE = r'#?([0-9A-Za-z]+(?:#|/)[0-9A-Za-z]+)'
 MESSAGE_COMMENT_RE = r'#?([0-9A-Za-z]+(?:(?:#|/)[0-9A-Za-z]+)?)'
 
+USER_REC = re.compile(USER_RE)
 MESSAGE_REC = re.compile(MESSAGE_RE)
 COMMENT_REC = re.compile(COMMENT_RE)
 MESSAGE_COMMENT_REC = re.compile(MESSAGE_COMMENT_RE)
@@ -27,6 +30,11 @@ def canonic_comment(s):
 def canonic_message_comment(s):
     m = MESSAGE_COMMENT_REC.match(s)
     return m.group(1).replace('#', '/') if m else ""
+
+
+def canonic_user(s):
+    m = USER_REC.match(s)
+    return m.group(1) if m else ""
 
 
 class CommandParserException(Exception):

bnw/handlers/command_bl.py
@@ -28,6 +28,7 @@ def cmd_blacklist(request, user="", tag="", club="", delete=""):
         )
 
     if user:
+        user = canonic_user(user)
         _ = yield bl_internal(request.user['name'], 'user', delete, user[:128])
     elif tag:
         _ = yield bl_internal(request.user['name'], 'tag', delete, tag[:256])

bnw/handlers/command_login.py
@@ -22,6 +22,7 @@ def cmd_passlogin(request, user=None, password=None):
     """ Логин паролем """
     if not (user and password):
         defer.returnValue(dict(ok=False, desc='Credentials cannot be empty.'))
+    user = canonic_user(user)
     u = yield objs.User.find_one({'name': user, 'settings.password': password})
     if u:
         defer.returnValue(dict(ok=True,

bnw/handlers/command_pm.py
@@ -12,7 +12,7 @@ from throttle import throttle_check
 def cmd_pm(request, text, user=""):
     """ Отправка приватного сообщения """
     yield throttle_check(request.user['name'])
-    user = user.lower()
+    user = canonic_user(user).lower()
     if len(text) > 2048:
         defer.returnValue(dict(ok=False, desc='Too long.'))
     target_user = yield objs.User.find_one({'name': user})

bnw/handlers/command_register.py
@@ -27,7 +27,7 @@ def cmd_register(request, name=""):
                      )
             )
         else:
-            name = name.lower()[:128]
+            name = canonic_user(name).lower()[:128]
             if name == 'anonymous':
                 defer.returnValue(
                     dict(ok=False, desc=u'You aren''t anonymous.')

bnw/handlers/command_show.py
@@ -121,7 +121,7 @@ def cmd_show(request, message='', user='', tag='', club='', page='0',
         parameters = [('tags', tag), ('clubs', club), ('id', message.upper())]
         parameters = dict(p for p in parameters if p[1])
         if user:
-            user = user.lower()
+            user = canonic_user(user).lower()
             if show == 'messages':
                 user_spec = dict(user=user)
             elif show == 'recommendations':

bnw/handlers/command_subscription.py
@@ -45,7 +45,7 @@ def cmd_subscriptions(request):
 @defer.inlineCallbacks
 def cmd_subscribe(request, message="", user="", tag="", club="", newtab=None):
         """ Подписка """
-        user = user.lower()
+        user = canonic_user(user).lower()
         message = canonic_message(message).upper()
         tag = tag.lower()
         club = club.lower()
@@ -70,7 +70,7 @@ def cmd_subscribe(request, message="", user="", tag="", club="", newtab=None):
 def cmd_unsubscribe(request, message="", user="", tag="", club="", newtab=None):
         """ Отписывание """
         # В этой функции DRY всосало по полной
-        user = user.lower()
+        user = canonic_user(user).lower()
         message = canonic_message(message).upper()
         tag = tag.lower()
         club = club.lower()

commit c4e6661c904c592ffe8a26bf86ddcd0c39f09cc2

Fix web interface
4c7f4c4189e65e6d254c383ef0dceebe56ca0271 broke things: anything that
matched [0-9A-Za-z]+ was considered to be a username and thus
highlighted.

bnw/formatting/linkshit_format.py
@@ -10,7 +10,9 @@ from libthumbor import CryptoURL
 from bnw.core.base import config
 from bnw.handlers.base import USER_RE
 
-_USER_RE = re.compile(ur"""(?:(?<=[\s\W])|^)%s""" % USER_RE)
+# we strip first two characters of USER_RE, i.e. "@?", because we *require* "at
+# sign" to be present
+_USER_RE = re.compile(ur"""(?:(?<=[\s\W])|^)@%s""" % USER_RE[2:])
 
 bnw_types = (
     ('msg', _MSG_RE, lambda m: (m.group(1),)),